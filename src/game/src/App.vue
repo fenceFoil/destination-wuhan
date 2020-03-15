b2981aa21ee848846fb12841c5b5ba70c558d5a5
Michael Pedersen <michaelp@discoverdst.com>
2020-03-14 22:30:35 -0500

    badly scaled paper

--- src/game/src/App.vue
+++ src/game/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome Reporter, to Wuhan, China"/>
   </div>
 </template>
@@ -25,4 +24,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+html {
+  background-image: url("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Qez5lj45fXjyab8waBAVUwHaEq%26pid%3DApi&f=1");
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  overflow:hidden;
+}
+
 </style>
--- src/game/src/components/HelloWorld.vue
+++ src/game/src/components/HelloWorld.vue
@@ -1,34 +1,8 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="paper-container">
+    <textarea class="paper" rows="30" cols="80">
+    </textarea>
+    </div>
 </template>
 
 <script>
@@ -42,6 +16,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
 h3 {
   margin: 40px 0 0;
 }
@@ -55,5 +30,56 @@ li {
 }
 a {
   color: #42b983;
+}
+.paper-background {
+  
+  
+}
+
+.paper-container{
+  transform: scale3d(.25,.25,.25);
+  position:absolute;
+  top:-20%;
+  left:0%;
+  right:-20%;
+  bottom:0%;
+}
+
+.paper{
+  padding: 9em 10em;
+  font-size: 3em;
+  line-height: 2em;
+  overflow: hidden;
+  font-family: 'Caveat', cursive;
+  background-image: url("https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ffarm8.staticflickr.com%2F7192%2F6796259354_79b96a7234_o.jpg");
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  
+  opacity:1;
+  transform: rotate3d(1, 1, 1, -45deg);
+
+/*  -moz-animation-name: spin;
+    -moz-animation-duration: 4000ms;
+    -moz-animation-iteration-count: infinite;
+    -moz-animation-timing-function: linear;
+
+    @-moz-keyframes spin { 
+        from { 
+            -moz-transform: rotate3d(1, 0, 1, 0deg);
+        } to { 
+            -moz-transform: rotate3d(0, 1, 1, 360deg);
+        }
+    }
+    @keyframes spin { 
+        from { 
+            transform: rotate3d(1, 0, 1, 0deg);
+        } to { 
+            transform: rotate3d(0, 1, 1, 360deg);
+        }
+    }*/
 }
 </style>
